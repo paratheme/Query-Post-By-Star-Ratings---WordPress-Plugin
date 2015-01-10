179d8be5379a58a3c0fab6e386edaa3270b9a00d
nurhasan <public.nurhasan@gmail.com>
2015-01-10 21:16:34 +0600

    query-post-by-ratings.php update

--- query-post-by-ratings.php
+++ query-post-by-ratings.php
@@ -1,57 +1,61 @@
 <?php
 
 
-		$post_type = 'post';
-		$post_status = 'publish';
-		$posts_per_page = 5;
-				
-		$html = '';
-		
-		$wp_query = new WP_Query(
-			array (
-				'post_type' => $post_type,
-				'post_status' => $post_status,
-				'posts_per_page' => $posts_per_page,
-				'meta_query' => array(
-						array(
-							'key' => 'totalstarvalue',
-							'value' => 0,
-							'compare' => '>',
-							'type' => 'NUMERIC'
-							),
+	//Author: ParaTheme
+	//Website: http://paratheme.com
 
-					)
-				) );
-						
-						
-		if ( $wp_query->have_posts() ) :
-		
-		while ( $wp_query->have_posts() ) : $wp_query->the_post();
 
-			$all_rate[get_the_ID()] = get_post_meta( get_the_ID(), 'totalstarvalue', true );
-			$all_count[get_the_ID()] = get_post_meta( get_the_ID(), 'totalstarcount', true );	
-			$post_rates[get_the_ID()] = 	$all_rate[get_the_ID()]/$all_count[get_the_ID()];		
-				
-		endwhile;
-		
-		wp_reset_query();
-		endif;				
-		
-		
-		$html .= '<br />';
+	$post_type = 'post';
+	$post_status = 'publish';
+	$posts_per_page = 5;
+			
+	$html = '';
+	
+	$wp_query = new WP_Query(
+		array (
+			'post_type' => $post_type,
+			'post_status' => $post_status,
+			'posts_per_page' => $posts_per_page,
+			'meta_query' => array(
+					array(
+						'key' => 'totalstarvalue',
+						'value' => 0,
+						'compare' => '>',
+						'type' => 'NUMERIC'
+						),
 
-		
-		arsort($post_rates);
+				)
+			) );
+					
+					
+	if ( $wp_query->have_posts() ) :
+	
+	while ( $wp_query->have_posts() ) : $wp_query->the_post();
 
-		foreach($post_rates as $key => $value)
-			{
-				$post = get_post($key);
-				$html .= $post->post_title.' - '.$value.'<br />';
-				
-				// you content display here
-				// Please read more about get_post http://codex.wordpress.org/Function_Reference/get_post
-			}
+		$all_rate[get_the_ID()] = get_post_meta( get_the_ID(), 'totalstarvalue', true );
+		$all_count[get_the_ID()] = get_post_meta( get_the_ID(), 'totalstarcount', true );	
+		$post_rates[get_the_ID()] = 	$all_rate[get_the_ID()]/$all_count[get_the_ID()];		
+			
+	endwhile;
+	
+	wp_reset_query();
+	endif;				
+	
+	
+	$html .= '<br />';
 
-		
-		return $html;
+	
+	arsort($post_rates);
+
+	foreach($post_rates as $key => $value)
+		{
+			$post = get_post($key);
+			$html .= $post->post_title.' - '.$value.'<br />';
+			
+			// you content display here
+			// Please read more about get_post http://codex.wordpress.org/Function_Reference/get_post
+		}
+
+	
+	return $html;
 ?>
